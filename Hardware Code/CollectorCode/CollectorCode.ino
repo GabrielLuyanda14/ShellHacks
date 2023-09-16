df6dfc619fb572907eff5cbb61e4e19d103028fe
Gabriel Luyanda <Gabriel.Luyanda14@gmail.com>
2023-09-16 03:00:29 -0400

    Sender Code First Draft

--- Hardware Code/CollectorCode/CollectorCode.ino
+++ Hardware Code/CollectorCode/CollectorCode.ino
@@ -1,9 +1,50 @@
+#include <SparkFunCCS811.h>
+
+#define button 7
+#define GreenLED 2
+#define RedLED 4
+#define CCS811_ADDR 0x5B //Default I2C Address
+//#define CCS811_ADDR 0x5A //Alternate I2C Address
+
+CCS811 myCCS811(CCS811_ADDR);
+
 void setup() {
   // put your setup code here, to run once:
+  myCCS811.begin();
 
+  Serial.begin(9600);
+  Serial.println("Starting");
+
+  pinMode(button, INPUT_PULLUP);
+  pinMode(GreenLED, OUTPUT);
+  pinMode(RedLED, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if(digitalRead(button) == HIGH){
+    if (myCCS811.dataAvailable())
+    {
+      myCCS811.readAlgorithmResults();
+      int tempCO2 = myCCS811.getCO2();
+      int tempVOC = myCCS811.getTVOC();
+      Serial.print(String(tempCO2)+","+String(tempVOC)+",");
+    }
+    else if (myCCS811.checkForStatusError())
+    {
+      //Serial.print("Fuck you");
+      //while(1);
+    }
+  
+    Serial.println(((analogRead(A0))));// * (3300/1024))-500)/10);
 
+    digitalWrite(GreenLED, HIGH);
+    digitalWrite(RedLED, LOW);
+    
+  }else{
+    Serial.println("255,1024,250");
+    digitalWrite(GreenLED, LOW);
+    digitalWrite(RedLED, HIGH);
+  }
+  delay(1000); //Wait for next reading
 }
